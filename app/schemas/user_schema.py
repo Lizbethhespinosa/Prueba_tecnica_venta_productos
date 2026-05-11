0613a6fc57159515e2cc931334d51637933b248b
Lizbeth <lizbeth.espinosa@udea.edu.co>
2026-05-11 15:38:40 -0500

    CommitN3 CRUD USERS
    CRUD ccompleto users

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,6 +1,24 @@
 from pydantic import BaseModel, EmailStr
 
+# CREAR USUARIO
 class UserCreate(BaseModel):
+    nombre: str
+    email: EmailStr
+    password: str
+
+
+# RESPUESTA USUARIO
+class UserResponse(BaseModel):
+    id: int
+    nombre: str
+    email: EmailStr
+
+    class Config:
+        from_attributes = True
+
+
+# ACTUALIZAR USUARIO
+class UserUpdate(BaseModel):
     nombre: str
     email: EmailStr
     password: str
