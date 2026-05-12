a9a018a79021102e85f3d40790ca242777b72a62
Lizbeth <lizbeth.espinosa@udea.edu.co>
2026-05-11 19:22:28 -0500

    Commit  carpetas y hashing
    Organización carpeta backend y actualización hashing

--- backend/app/utils/security.py
+++ backend/app/utils/security.py
@@ -1,9 +1,11 @@
 from passlib.context import CryptContext
 
+
 pwd_context = CryptContext(
-    schemes=["bcrypt"],
+    schemes=["pbkdf2_sha256"],
     deprecated="auto"
 )
+
 
 def hash_password(password: str):
 
